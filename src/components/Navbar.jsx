3aa3ea1e2360263f18e2389920f4afaa6efee405
Achiever-P <153370149+Achiever-P@users.noreply.github.com>
2025-05-10 08:23:04 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const NavBar = ({ darkMode, setDarkMode }) => {
             {/* Logo */}
             <div className="flex items-center gap-4">
               <img
-                  src="/public/img/logo.png"
+                  src="/public/logo.png"
                   alt="logo"
                   className="h-10 md:h-12 object-contain max-h-full"
               />
